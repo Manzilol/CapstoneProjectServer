0613a0a5f1b293ec198cabe266d8f99f8dbf7011
Stewart Manzi <stewartmanzi@gmail.com>
2022-11-01 10:16:25 +0000

    added spring

---
+++ VeloVault/src/main/java/com/example/VeloVault/VeloVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.VeloVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloVaultApplication.class, args);
+	}
+
+}
