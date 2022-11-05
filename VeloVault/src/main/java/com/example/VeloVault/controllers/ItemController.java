516e7ee027e13cb5a1562b7950a182fb39ee8954
Stewart Manzi <stewartmanzi@gmail.com>
2022-11-05 11:33:55 +0000

    fixed controller bug

--- VeloVault/src/main/java/com/example/VeloVault/controllers/ItemController.java
+++ VeloVault/src/main/java/com/example/VeloVault/controllers/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class ItemController {
     @GetMapping(value = "/items")
     public List<Item> getAllItems(){ return itemRepository.findAll();}
 
-    @GetMapping(value = "items")
+    @PostMapping(value = "/items")
     public ResponseEntity postItem(@RequestBody Item item){
         itemRepository.save(item);
         return new ResponseEntity<>(item, HttpStatus.CREATED);
