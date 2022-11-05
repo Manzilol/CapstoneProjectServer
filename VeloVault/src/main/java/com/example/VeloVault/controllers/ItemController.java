c08cf639a9d86d1251c81fc156f0ee8aaa74f772
Stewart Manzi <stewartmanzi@gmail.com>
2022-11-05 10:48:13 +0000

    added get all and post new to all controllers

--- VeloVault/src/main/java/com/example/VeloVault/controllers/ItemController.java
+++ VeloVault/src/main/java/com/example/VeloVault/controllers/ItemController.java
@@ -3,7 +3,10 @@ package com.example.VeloVault.controllers;
 import com.example.VeloVault.models.Item;
 import com.example.VeloVault.repositories.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,9 @@ public class ItemController {
     @GetMapping(value = "/items")
     public List<Item> getAllItems(){ return itemRepository.findAll();}
 
-
+    @GetMapping(value = "items")
+    public ResponseEntity postItem(@RequestBody Item item){
+        itemRepository.save(item);
+        return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
 }
