f5600b62c307e7009eae9f97822f14b0b0ccda1a
Bruce Johnston <brucejohnston@ymail.com>
2022-11-06 09:02:50 +0000

    datepicker & and booking controller end points

--- VeloVault/src/test/java/com/example/VeloVault/VeloVaultApplicationTests.java
+++ VeloVault/src/test/java/com/example/VeloVault/VeloVaultApplicationTests.java
@@ -1,17 +1,21 @@
 package com.example.VeloVault;
 
+import com.example.VeloVault.models.Booking;
 import com.example.VeloVault.models.Item;
 import com.example.VeloVault.models.User;
 import com.example.VeloVault.models.mainCatagory.subCatagory.Insulated;
+import com.example.VeloVault.repositories.BookingRepository;
 import org.junit.Before;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,6 +26,9 @@ public class VeloVaultApplicationTests {
 
 	User user;
 	Item insulated;
+
+	@Autowired
+	private BookingRepository repository;
 
 
 //	@Before
@@ -40,5 +47,11 @@ public class VeloVaultApplicationTests {
 //		insulated.rentItem(user, insulated, "1/1/2022", "3/1/2022");
 //		assertEquals(1, insulated.getBookingListSize());
 //	}
+
+	@Test
+	public void getById() throws Exception {
+		Optional<Booking> b = repository.findById(1L);
+		System.out.println(b.get());
+	}
 
 }
